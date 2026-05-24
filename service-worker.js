95785bcb8cad8ad13ad208dc1658d7de4cfbafe9
rexxxy-np <sameerthapa90809@gmail.com>
2026-05-24 17:19:52 +0545

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,20 @@
-const CACHE = 'voxa-v1';
-const ASSETS = [
-  '/voxa-backhend/',
-  '/voxa-backhend/index.html',
-  '/voxa-backhend/manifest.json'
-];
+const CACHE = 'voxa-v2';
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
